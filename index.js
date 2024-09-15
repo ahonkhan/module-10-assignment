35c69dee4749e6148ad2b5aa7e5080fbe4991f6c
ahonkazi <aohinuzzaman420@gmail.com>
2024-09-15 11:13:21 +0600

    function call done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const isOdd = (num) => {
 const findMin = (array) => {
   let min = array[0];
 
-  for (let i = 1; i < array.length - 1; i++) {
+  for (let i = 1; i < array.length; i++) {
     if (min > array[i]) {
       min = array[i];
     }
@@ -31,8 +31,7 @@ const filterEvenNumbers = (array) => {
   return newArray;
 };
 
-const sortArrayDescending = () => {
-  let array = [5, 4, 1, 8, 2];
+const sortArrayDescending = (array) => {
   for (let j = 0; j < array.length; j++) {
     for (let i = 0; i < array.length; i++) {
       if (array[i] < array[i + 1]) {
@@ -85,4 +84,34 @@ const findAverage = (array) => {
   return sum / array.length;
 };
 
+// calculating difference
+const difference = calculateDifference(15, 5);
+console.log(difference);
 
+// checking is odd
+const checkIsOdd = isOdd(5);
+console.log(checkIsOdd);
+
+// find min
+const min = findMin([8, 5, 9, 100, 2]);
+console.log(min);
+
+// filterEvenNumbers
+const evenNumbers = filterEvenNumbers([8, 5, 9, 100, 2]);
+console.log(evenNumbers);
+
+// sortArrayDescending
+const arrayDesc = sortArrayDescending([8, 5, 9, 100, 2]);
+console.log(arrayDesc);
+
+// lowercaseFirstLetter
+const lcfirst = lowercaseFirstLetter("AHON KHAN");
+console.log(lcfirst);
+
+// countVowels
+const vowel_count = countVowels("Ahon Khan");
+console.log(vowel_count);
+
+// findAverage
+const avg = findAverage([8, 5, 6, 4]);
+console.log(avg);
